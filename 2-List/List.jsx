07516fe876c7d5ed3278387132801dee4326a124
odetayo dorcas <odetayodorcas123@gmail.com>
2024-04-22 18:13:23 +0100

    Update List.jsx

--- 2-List/List.jsx
+++ 2-List/List.jsx
@@ -1,1 +1,25 @@
 
+function List() {
+  const students = [
+    {
+      name: "Doe",
+      age: "18"
+    },
+    {
+      name: "sam",
+      age: "20"
+    }
+  ]
+  return (
+    <>
+      {students.map((student) => (
+        <ul key={Math.random() * 10}>
+          <li>{student.name}</li>
+          <li>{student.age}</li>
+        </ul>
+      ))}
+    </>
+  )
+}
+
+export default List
