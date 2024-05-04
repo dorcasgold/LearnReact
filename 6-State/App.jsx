cff23ed0441f4d32d2e74ff3f14a12253298718a
odetayo dorcas <odetayodorcas123@gmail.com>
2024-05-04 04:59:58 +0100

    Create App.jsx

---
+++ 6-State/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
+  )
+}
+
+function List() {
+  return (
+    <Counter />
+  )
+}
+export default List
