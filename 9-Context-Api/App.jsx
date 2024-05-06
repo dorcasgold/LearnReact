f5cd951073c09a9f9601f0788bc1701f03d8802c
odetayo dorcas <odetayodorcas123@gmail.com>
2024-05-06 17:59:55 +0100

    Update App.jsx

--- 9-Context-Api/App.jsx
+++ 9-Context-Api/App.jsx
@@ -1,1 +1,23 @@
+import React, { useContext } from 'react'; 
+import './App.css';
 
+export const MyContext = React.createContext();
+
+const ChildComponent = () => {
+  const contextValue = useContext(MyContext);
+  return (
+    <h1>ChildComponent {contextValue}</h1>
+  );
+};
+
+function App() {
+  const sharedValue = 'Hello from Context';
+
+  return (
+    <MyContext.Provider value={sharedValue}>
+      <ChildComponent />
+    </MyContext.Provider>
+  );
+}
+
+export default App;
